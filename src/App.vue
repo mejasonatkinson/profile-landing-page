85cdc628ad65f86e66c7ebcf7854f99ce501ac60
mejasonatkinson <me@jasonatkinson.co.uk>
2024-03-31 22:01:17 +0100

    Add Tailwind CSS, with basic demo

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,59 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue Profile Landing Page" />
+<div class="min-h-screen w-full">
+        <div class="w-full flex border-b border-slate-300">
+            <!-- Top Space -->
+            <div class="w-1/12 border-r border-slate-300">
+                <!-- Left Space -->
+            </div>
+            <div class="w-full h-14">
+
+            </div>
+            <div class="w-1/12 border-l border-slate-300">
+                <!-- right Space -->
+            </div>
+        </div>
+        <div class="w-full flex">
+            <div class="w-1/12 border-r border-slate-300">
+                <!-- Left Space -->
+            </div>
+            <div class="w-full">
+                <div class="w-full border-b border-slate-300">
+                    <h1 class="text-6xl p-2 pb-12 text-red-600">
+                        Jason Atkinson, 
+                        <span class="text-teal-500" id="title">
+                            Creative Developer
+                        </span>
+                    </h1>
+                </div>
+                <div class="w-full">
+                    <p class="text-4xl p-2 pb-8 leading-relaxed" id="description">
+                        At my core I am a creative problem solver. This passion and creative need to understand, is the driving force behind everything which I do.
+                    </p>
+                </div>
+            </div>
+            <div class="w-1/12 border-l border-slate-300">
+                <!-- right Space -->
+            </div>
+        </div>
+        <div class="w-full flex border-t border-slate-300">
+            <!-- Bottom Space -->
+            <div class="w-1/12 border-r border-slate-300">
+                <!-- Left Space -->
+            </div>
+            <div class="w-full h-14">
+     
+            </div>
+            <div class="w-1/12 border-l border-slate-300">
+                <!-- right Space -->
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
