6fb41024bc0a78617b9dbdb7888f65afee26d241
mejasonatkinson <me@jasonatkinson.co.uk>
2024-04-14 13:35:29 +0100

    update darkmode feature

--- src/App.vue
+++ src/App.vue
@@ -55,6 +55,14 @@ const lightDarkMode = () => {
 </script>
 
 <template>
+<span :onclick="lightDarkMode" class="absolute top-0 right-0 cursor-pointer hover:opacity-75 border-b border-l p-4 border-brand_border print:hidden">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" :class="state.light ? 'inline' : 'hidden'">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white" :class="state.light ? 'hidden' : 'inline'">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+    </svg>
+</span>
 <div class="
     bg-brand_bg animate-bg-fade text-brand_text 
     min-h-screen h-screen max-h-full w-screen flex flex-col
@@ -62,10 +70,6 @@ const lightDarkMode = () => {
     <div class="w-full h-full">
         <div class="flex w-8/12 h-full m-auto border-x border-brand_border">
             <div class="block w-full m-auto print:border-y lg:border-y text-base text-right border-brand_border">
-                <span class="inline-block font-semibold p-2 print:hidden">
-                    <span>//</span> <span class="cursor-pointer hover:opacity-75 transition ease-in-out"
-                        :onclick="lightDarkMode">{{ state.light ? "Dark Mode" : "Light Mode" }}</span>
-                </span>
                 <span class="inline-block font-semibold p-2 ">
                     <span>//</span> <span class="text-brand_primary">Email:</span> <a :href="'mailto:' + contentStore.email"
                         class="text-brand_secondary cursor-pointer hover:opacity-75 transition ease-in-out">{{
